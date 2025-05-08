5cf5785bfe4096897386e7f1c932b4d7cd57e42f
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-05-07 23:23:30 -0300

    chore: creating an alias for the components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Calculator } from "./components/Calculator";
-import { OperationHistory } from "./components/OperationHistory";
+import { Calculator } from "@components/Calculator";
+import { OperationHistory } from "@components/OperationHistory";
 
 function App() {
   return (
