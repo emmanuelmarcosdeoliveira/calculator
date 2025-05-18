698050ea71c12300ea9edd5370fba9938c10d6ec
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-05-17 22:22:27 -0300

    chore: The Calculator component has had its structure modified, rendering elements through lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { Calculator } from "@components/Calculator";
-import { OperationHistory } from "@components/OperationHistory";
-
+import { Calculator } from "./components/Calculator";
+import { OperationHistory } from "./components/OperationHistory";
 function App() {
   return (
     <>
       <main
         className={`font-(--font) min-h-screen bg-linear-[180deg,_#807ece_0%,_#8e7ece_100%] items-center  flex gap-2  flex-col sm:flex-row  sm:justify-center p-8   text-(--text) `}
       >
-        <Calculator />
+        <Calculator copy />
         <OperationHistory />
       </main>
     </>
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,3 +1,4 @@
+import { ButtonsCalc } from "../constants/contants";
 import { Button } from "./Button";
 import { CalculatorDisplay } from "./CalculatorDisplay";
 import { Container } from "./Container";
@@ -7,46 +8,19 @@ export function Calculator() {
     <Container className="flex flex-col gap-[1.625rem] pb-8 pt-14 px-8 w-[22.25rem]">
       <CalculatorDisplay operation="1+1" result="2" />
       <div className="flex flex-col gap-3">
-        <div className="flex gap-3">
-          <Button className="h-16 w-16">CE</Button>
-          <Button className="flex-1 h-16">C</Button>
-          <Button variant="primary" className="h-16 w-16">
-            ÷
-          </Button>
-        </div>
-        <div className="flex gap-3">
-          <Button className="h-16 w-16">7</Button>
-          <Button className="h-16 w-16">8</Button>
-          <Button className="h-16 w-16">9</Button>
-          <Button variant="primary" className="h-16 w-16">
-            x
-          </Button>
-        </div>
-        <div className="flex gap-3">
-          <Button className="h-16 w-16">4</Button>
-          <Button className="h-16 w-16">5</Button>
-          <Button className="h-16 w-16">6</Button>
-          <Button variant="primary" className="flex h-16 justify-center w-16">
-            {" "}
-            <span className="bg-(--text) h-0.5 inline-block w-3"></span>
-          </Button>
-        </div>
-        <div className="flex gap-3">
-          <Button className="h-16 w-16">1</Button>
-          <Button className="h-16 w-16">2</Button>
-          <Button className="h-16 w-16">3</Button>
-          <Button variant="primary" className="h-16 w-16">
-            +
-          </Button>
-        </div>
-        <div className="flex gap-3">
-          <Button className="flex-1 h-16 w-16">0</Button>
-          <Button className="h-16 w-16">,</Button>
-
-          <Button variant="primary" className="bg-purple-600/90 h-16 w-16">
-            =
-          </Button>
-        </div>
+        {ButtonsCalc.map((row, index) => (
+          <div key={index} className="flex gap-3">
+            {row.map((button) => (
+              <Button
+                key={button.input}
+                className={button.className || "h-16 w-16"}
+                variant={button.variant}
+              >
+                {button.input}
+              </Button>
+            ))}
+          </div>
+        ))}
       </div>
     </Container>
   );
